c833afcc8623b6fa05a9e1cf4316b7c8f4f5a716
Chill-Lee <1361576187@qq.com>
2021-07-09 22:32:19 +0800

    新增cloud-provider-payment8004模块,测试zookeeper注册中心

---
+++ cloud-provider-payment8004/src/main/java/org/lee/springcloud/PaymentMain8004.java
@@ -0,0 +1,17 @@
+package org.lee.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Lee
+ * @date 2021年07月09日 16:15
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class, args);
+    }
+}
---
+++ cloud-provider-payment8004/src/main/java/org/lee/springcloud/controller/PaymentController.java
@@ -0,0 +1,26 @@
+package org.lee.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author chill
+ * @date 2021年07月09日 16:17
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/zk")
+    public String paymentzk()
+    {
+        return "springcloud with zookeeper: "+serverPort+"\t"+ UUID.randomUUID().toString();
+    }
+}
