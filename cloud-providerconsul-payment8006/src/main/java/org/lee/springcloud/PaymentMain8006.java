e4479bdc8c7ef862223680f98a0f6d4b706da01a
Chill-Lee <1361576187@qq.com>
2021-07-15 16:54:03 +0800

    cloud-providerconsul-payment8006 init

---
+++ cloud-providerconsul-payment8006/src/main/java/org/lee/springcloud/PaymentMain8006.java
@@ -0,0 +1,17 @@
+package org.lee.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author chill
+ * @date 2021年07月15日 15:35
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8006 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8006.class, args);
+    }
+}
